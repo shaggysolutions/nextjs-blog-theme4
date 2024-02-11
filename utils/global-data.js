2f4fe847517d4efad742056406e5944cef83af43
shaggysolutions <109769808+shaggysolutions@users.noreply.github.com>
2024-02-11 15:35:03 -0600

    Update global-data.js
    added blog tittle - a premium pool care company

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'SweetwaterPool Cleaning';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'A Premium Pool Care Company';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
